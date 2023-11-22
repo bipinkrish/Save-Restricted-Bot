aa5d4374d4b1ec90fb91028e5fa4e98203e9aca5
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-11-23 01:01:03 +0530

    Update and rename config.json to config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+class Config(object):
+    # pyro client config
+    API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
+    API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
+    STRING_SESSION = os.environ.get("STRING_SESSION", "") # ⚠️ Required
