00a37d606ba2c6d6fefa4c7c228740d89adc3500
Snowball <108003920+Snowball-0@users.noreply.github.com>
2023-11-23 11:38:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ class Config(object):
     API_ID    = os.environ.get("API_ID", "")  # ⚠️ Required
     API_HASH  = os.environ.get("API_HASH", "") # ⚠️ Required
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "") # ⚠️ Required
+    ADMIN = int(os.environ.get("ADMIN, "")) # ⚠️ Required
     STRING_SESSION = os.environ.get("STRING_SESSION", "") # ⚠️ Required
     PORT = int(os.environ.get("PORT", "8080"))
